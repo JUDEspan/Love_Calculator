3a396d7a13f9db12077acc3828bf2672088e15e8
Odede Jude <judeodede@gmail.com>
2023-10-31 12:28:12 +0100

    fist commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const check = () => {
+    let one = first.value
+    let two =second.value   
+    //alert("i dey work")
+    if(one=="" && two==""){
+        err.innerHTML= `<p style="color:red; ">Please fill all </p>`
+    }else{
+        document.getElementById("first").value=""
+        second.value=""
+        err.innerHTML=""
+        show.innerHTML=""
+        let mat = Math
+        let rando = mat.random()*100
+        let ans = mat.round(rando)
+    show.innerHTML=`<p>Love score for ${one} and ${two} is ${ans}% </p>`
+    }
+
+    if(ans>40){
+        remark.innerHTML=`<p>please na beg i dey beg no enter ham
+        .</p>`
+    }
+}
