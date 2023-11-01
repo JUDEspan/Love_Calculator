58cf0eab7fe221f3620612420ab0e4a9ecfc6eb6
Odede Jude <judeodede@gmail.com>
2023-11-01 12:42:35 +0100

    morning update commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const check = () => {
     let two =second.value   
     //alert("i dey work")
     if(one=="" && two==""){
-        err.innerHTML= `<p style="color:red; ">Please fill all </p>`
+        err.innerHTML= `<p style="color:white; background-color:red; width:200px; border-radius:20px;">Please fill all </p>`
     }else{
         document.getElementById("first").value=""
         second.value=""
@@ -12,11 +12,11 @@ const check = () => {
         let mat = Math
         let rando = mat.random()*100
         let ans = mat.round(rando)
-    show.innerHTML=`<p>Love score for ${one} and ${two} is ${ans}% </p>`
+    show.innerHTML=`<p style="background-color:red">Love score for ${one} and ${two} is ${ans}% </p>`
     }
 
     if(ans>40){
-        remark.innerHTML=`<p>please na beg i dey beg no enter ham
+        remark.innerHTML=`<p >please na beg i dey beg no enter ham
         .</p>`
     }
 }
